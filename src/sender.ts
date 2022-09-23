342cec2349aad1dc4ca1f16f9c2a8c3615fc7ff1
Sajahh <mariavaicomoutras@hotmail.com>
2022-09-23 16:42:58 -0300

    feat: classe sender

--- src/sender.ts
+++ src/sender.ts
@@ -1,9 +1,37 @@
-const {create, Whatsapp, Message, SocketState} = require("venom-bot");
+import {start} from 'repl';
+import { Whatsapp, create, Message, SocketState } from "venom-bot";
 
 
 class Sender {
     private client: Whatsapp;
-}
+    
+
     constructor() {
-        this.client = create();
+        this.initialize();
+    };
+
+  async sendText(to: string, body: string) {
+      //this.sendText("5571988928500@c.us", "Essa mensagem foi enviada pelo bot");
+    this.client.sendText(to, body);
     }
+
+    private initialize() {
+        const qr = (base64Qrimg: string) => {};
+
+        const status = (statusSession: string ) => {};
+
+       const start = (client: Whatsapp) => {
+            this.client = client;
+            console.log("Client is ready")
+             
+        };
+
+    create("wpp-sender",qr, status)
+        .then((client: Whatsapp) => start(client))
+        .catch((erro: string) => {
+            console.log(erro);
+        }
+        )}
+    };
+
+    export default Sender;
